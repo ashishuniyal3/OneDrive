0eef5948cfcfdec815134539ddfd93053cf33d05
Webwerks <webwerks@Ashish.local>
2015-06-23 15:38:41 +0530

    Initial Commit

---
+++ OneDrive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OneDrive
+//
+//  Created by Webwerks on 6/23/15.
+//  Copyright (c) 2015 Webwerks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OneDriveTests/OneDriveTests.m
@@ -0,0 +1,40 @@
+//
+//  OneDriveTests.m
+//  OneDriveTests
+//
+//  Created by Webwerks on 6/23/15.
+//  Copyright (c) 2015 Webwerks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface OneDriveTests : XCTestCase
+
+@end
+
+@implementation OneDriveTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
